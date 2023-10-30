a6eed49a2b7298d27b82fa529142b0d744ac882a
thePlaceholder <68772026+someElseIsHere@users.noreply.github.com>
2023-10-30 19:23:52 +0000

    ftdrfg

--- common/src/main/java/org/theplaceholder/sonicboom/Config.java
+++ common/src/main/java/org/theplaceholder/sonicboom/Config.java
@@ -1,9 +1,20 @@
 package org.theplaceholder.sonicboom;
 
+import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 
 @me.shedaniel.autoconfig.annotation.Config(name = "sonicboom")
 public class Config implements ConfigData {
     public float explosionSpeed = 35f;
     public float explosionThresholdSpeed = 30f;
+    
+    public static void register(){
+        AutoConfig.register(Config.class, GsonConfigSerializer::new);
+        Config config = AutoConfig.getConfigHolder(Config.class).getConfig();
+        SonicBoom.explosionSpeed = config.explosionSpeed;
+        SonicBoom.explosionThresholdSpeed = config.explosionThresholdSpeed;
+    
+    }
+    
 }
--- common/src/main/java/org/theplaceholder/sonicboom/SonicBoom.java
+++ common/src/main/java/org/theplaceholder/sonicboom/SonicBoom.java
@@ -1,21 +1,12 @@
 package org.theplaceholder.sonicboom;
 
-import me.shedaniel.autoconfig.AutoConfig;
-import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
-
 public class SonicBoom {
-    public static boolean isClothConfigPresent;
+    public static float explosionSpeed = 35f;
     public static float explosionThresholdSpeed = 30f;
 
     public static void init() {
-        isClothConfigPresent = isClothConfigPresent();
-        if(!isClothConfigPresent)
-            return;
-
-        AutoConfig.register(Config.class, GsonConfigSerializer::new);
-        Config config = AutoConfig.getConfigHolder(Config.class).getConfig();
-        explosionSpeed = config.explosionSpeed;
-        explosionThresholdSpeed = config.explosionThresholdSpeed;
+        if(isClothConfigPresent())
+            Config.register();
     }
 
     public static boolean isClothConfigPresent() {
